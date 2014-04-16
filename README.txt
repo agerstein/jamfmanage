c8832a4ee060c818a5360e1ef7f6e6b5e60b3b19
Adam Gerstein <adam@gerstein.net>
2014-04-16 09:10:16 -0400

    First sync
    script written, works, sync it.

---
+++ README.txt
@@ -0,0 +1,4 @@
+This will run the jamf binary with the "manage" argument.
+
+Adam Gerstein, gersteina1@southernct.edu
+2014-04-16 Creation of script
---
+++ jamfmanage.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+################################################################### 
+# This will run the jamf binary with the "manage" argument.
+#
+# Adam Gerstein, gersteina1@southernct.edu
+# 2014-04-16 Creation of script
+sudo /usr/sbin/jamf manage
